32e8c4b0592839f8d404b026716e61b1d13d6a39
OSCAR <OSCAR@LAPTOP-NHNT9S7U>
2023-11-25 01:24:29 -0600

    Agregar archivos de proyecto.

---
+++ CRUDMVCREACT/ClientApp/src/App.js
@@ -0,0 +1,122 @@
+﻿import { Container, Row, Col, Card, CardHeader, CardBody, Button } from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import TablaContacto from "./components/TablaContacto";
+import { useEffect, useState } from "react";
+import ModalContacto from "./components/ModalContacto";
+
+
+const App = () => {
+
+    const [contactos, setContactos] = useState([]);
+
+    const [mostrarModal, setMostrarModal] = useState(false);
+
+    const [editar, setEditar] = useState(null);
+
+    /**
+     * Servicios API
+     */
+
+    //Devuelve los valores de la tabla Contacto
+    const mostrarContactos = async () => {
+        const response = await fetch("api/contacto/lista");
+
+        if (response.ok) {
+            const data = await response.json();
+            setContactos(data);
+        }
+    }
+
+    //Guarda un nuevo registro en la tabla Contacto
+    const guardarContacto = async (contacto) => {
+        console.log("guardarContacto - App");
+        console.log(contacto);
+        const response = await fetch("api/contacto/guardar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify(contacto)
+        });
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal);
+            mostrarContactos();
+        }
+    }
+
+    //Editar registro en la tabla Contacto
+    const editarContacto = async (contacto) => {
+
+        const response = await fetch("api/contacto/editar", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify(contacto)
+        });
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal);
+            mostrarContactos();
+        }
+    }
+
+    //Eliminar registro en la tabla Contacto
+    const eliminarContacto = async (id) => {
+
+        var respuesta = window.confirm("¿Desea eliminar el contacto?");
+
+        if (!respuesta) {
+            return;
+        }
+
+        const response = await fetch("api/contacto/eliminar/" + id, {
+            method: "DELETE",
+        });
+
+        if (response.ok) {
+            mostrarContactos();
+        }
+    }
+
+    useEffect(() => {
+        mostrarContactos();
+    } , [])
+
+    return (
+    
+        <Container>
+            <Row className="mt-5">
+                <Col sm="12">
+                    <Card>
+                        <CardHeader>
+                            <h5>Lista de Contactos</h5>
+                        </CardHeader>
+                        <CardBody>
+                            <Button size="sm" color="success" onClick={() => setMostrarModal(!mostrarModal)}>Nuevo Contacto</Button>
+                            <hr></hr>
+                            <TablaContacto
+                                data={contactos}
+                                setEditar={setEditar}
+                                mostrarModal={mostrarModal}
+                                setMostrarModal={setMostrarModal}
+                                eliminarContacto={eliminarContacto}
+                            />
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <ModalContacto
+                mostrarModal={mostrarModal}
+                setMostrarModal={setMostrarModal}
+                guardarContacto={guardarContacto}
+                editar={editar}
+                editarContacto={editarContacto}
+            />
+        </Container>
+    
+    )
+}
+
+export default App;
---
+++ CRUDMVCREACT/ClientApp/src/components/ModalContacto.js
@@ -0,0 +1,76 @@
+﻿import { useEffect, useState } from "react";
+import { Table, Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from "reactstrap"
+
+const ModalContacto = ({ mostrarModal, setMostrarModal, guardarContacto, editar, editarContacto }) => {
+
+    const modeloContacto = {
+        idContacto: 0,
+        nombre: "",
+        correo: "",
+        telefono: ""
+    }
+
+    const [contacto, setContacto] = useState(modeloContacto);
+
+    const actualizaDato = (e) => {
+        setContacto({
+            ...contacto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const enviarDatos = () => {
+        if (contacto.idContacto == 0) {
+            console.log("enviarDatos - ModalComponent");
+            guardarContacto(contacto);
+            reiniciarModal();
+        }
+        else {
+            editarContacto(contacto);
+        }
+    }
+
+    const cerrarModal = () => {
+        setMostrarModal(!mostrarModal);
+        reiniciarModal();
+    }
+
+    const reiniciarModal = () => {
+        setContacto(modeloContacto);
+    }
+
+    useEffect(() => {
+        if (editar != null) {
+            setContacto(editar);
+        }
+        else {
+            setContacto(modeloContacto);
+        }
+    }, [editar])
+
+    return (
+        <Modal isOpen={ mostrarModal}>
+            <ModalHeader>
+                {contacto.idContacto == 0 ? "Nuevo Contacto" : "Editar Contacto"}
+            </ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label>Nombre</Label>
+                        <Input name="nombre" onChange={(e) => actualizaDato(e)} value={ contacto.nombre} />
+                        <Label>Correo</Label>
+                        <Input name="correo" onChange={(e) => actualizaDato(e)} value={contacto.correo} />
+                        <Label>Teléfono</Label>
+                        <Input name="telefono" onChange={(e) => actualizaDato(e)} value={contacto.telefono} />
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" size="sm" className="me-2" onClick={enviarDatos}>Guardar</Button>
+                <Button color="default" size="sm" onClick={cerrarModal}>Cancelar</Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
+
+export default ModalContacto;
